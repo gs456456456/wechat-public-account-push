ad16ccd4c7555ce71fcf3e8f59caad320fbff5e2
gs456456 <1154442437@qq.com>
2022-10-10 10:46:04 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'om7245jP8bjtHnlVECF5H8zVdjtI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Vsqhq7WSONmMNk8MDdrRTMtH9Co-TKZahzVBlejXKlI',
+      useTemplateId: '9aJhq9hkJgjNsESfadGdLS7le9p9O1jVnj5PfuaINg4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-02',
       festivals: [
@@ -45,7 +45,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'Vsqhq7WSONmMNk8MDdrRTMtH9Co-TKZahzVBlejXKlI',
+  CALLBACK_TEMPLATE_ID: '9aJhq9hkJgjNsESfadGdLS7le9p9O1jVnj5PfuaINg4',
 
   CALLBACK_USERS: [
     {
