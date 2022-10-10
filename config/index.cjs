4ac58e6da4bfc2131228d7cb68f7c50b58608574
gs456456 <1154442437@qq.com>
2022-10-10 10:50:45 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,17 +23,14 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'om7245jP8bjtHnlVECF5H8zVdjtI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '9aJhq9hkJgjNsESfadGdLS7le9p9O1jVnj5PfuaINg4',
+      useTemplateId: '7zCRq8sTRYoOCuCHvin2eo1vjh8mQsDZ1xlr-Zgqc5Y',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-02',
       festivals: [
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
           type: '生日', name: '宝宝', year: '1997', date: '11-02',
-        },
-        {
-          type: '节日', name: '恋爱纪念日', year: '2022', date: '04-05',
-        },
+        }
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
@@ -45,13 +42,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '9aJhq9hkJgjNsESfadGdLS7le9p9O1jVnj5PfuaINg4',
+  CALLBACK_TEMPLATE_ID: '7zCRq8sTRYoOCuCHvin2eo1vjh8mQsDZ1xlr-Zgqc5Y',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'om7245jP8bjtHnlVECF5H8zVdjtI',
+      id: '',
     }
   ],
 
